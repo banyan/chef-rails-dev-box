f3d829115e28b60109e47f55f1fe45f73811229e
Kohei Hasegawa <ameutau@gmail.com>
2013-03-18 22:20:20 +0900

    add base dependency for ruby and nokogiri or etc...

--- chef/site-cookbooks/base/recipes/default.rb
+++ chef/site-cookbooks/base/recipes/default.rb
@@ -2,3 +2,18 @@ execute "apt_update" do
  command "apt-get update"
  action :run
 end
+
+packages = %w(
+  curl
+  build-essential
+  libyaml-dev
+  libxml2
+  libxml2-dev
+  libxslt1-dev
+)
+
+packages.each do |p|
+  package p do
+    action :install
+  end
+end
